883f7802d820cac348711f9afe2bae9274d2a9e3
Exotik850 <kidkool850@gmail.com>
2020-02-14 06:20:54 -0800

    Added the first block

---
+++ src/main/java/com/exo/moreblocksv1/blocks/BlockBase.java
@@ -0,0 +1,33 @@
+package com.exo.moreblocksv1.blocks;
+
+import com.exo.moreblocksv1.main;
+import com.exo.moreblocksv1.init.ModBlocks;
+import com.exo.moreblocksv1.init.ModItems;
+import com.exo.moreblocksv1.util.IHasModel;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class BlockBase extends Block implements IHasModel
+{
+    public BlockBase(String name, Material material)
+    {
+        super(material);
+        setRegistryName(name);
+        setUnlocalizedName(name);
+        setCreativeTab(CreativeTabs.REDSTONE);
+
+        ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+
+    @Override
+    public void registerModels() 
+    {
+        main.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+
+}
---
+++ src/main/java/com/exo/moreblocksv1/init/ModBlocks.java
@@ -0,0 +1,17 @@
+package com.exo.moreblocksv1.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.exo.moreblocksv1.blocks.BlockBase;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class ModBlocks
+{
+    public static final List<Block> BLOCKS = new ArrayList<Block>();
+
+    public static final Block DINGA_BLOCK = new BlockBase("dinga_block", Material.SPONGE);
+
+}
